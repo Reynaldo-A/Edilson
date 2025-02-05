b5cf2d54eb2581b2baa82fe8a17ab0251e236d21
Reynaldo-A <reynaldocpcs@gmail.com>
2025-02-05 11:44:17 -0300

    Add files via upload

---
+++ login/protect.php
@@ -0,0 +1,9 @@
+<?php
+
+if(!isset($_SESSION)) {
+  session_start();
+}
+
+if(!isset($_SESSION['id'])) {
+  die("Você precisa estar logado para entrar nesta página.<p><a href=\"index.php\">Entrar</a></p>");
+}
